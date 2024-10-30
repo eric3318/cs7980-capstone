bd32609ec09f37c80cb16d9309159fa347fba5e2
Han Nie <nie.han@northeastern.edu>
2024-10-30 01:06:19 -0700

    added query parameters for methods in RoutingController

--- routing/src/main/java/org/shade/routing/controller/RoutingController.java
+++ routing/src/main/java/org/shade/routing/controller/RoutingController.java
@@ -5,24 +5,31 @@ import lombok.RequiredArgsConstructor;
 import org.shade.routing.service.RoutingService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class RoutingController {
 
   private final RoutingService routingService;
 
-  @RequestMapping(value = "/", method = RequestMethod.GET)
-  public ResponseEntity<?> getRoute() {
-    return new ResponseEntity<>(routingService.getRoute(), HttpStatus.OK);
+  @GetMapping("/edges")
+  public ResponseEntity<?> getEdges(@RequestParam double minLon, @RequestParam double maxLon,
+      @RequestParam double minLat, @RequestParam double maxLat) {
+    return new ResponseEntity<>(routingService.getEdges(minLon, maxLon, minLat, maxLat),
+        HttpStatus.OK);
   }
 
-  @RequestMapping(value = "/box", method = RequestMethod.GET)
-  public ResponseEntity<?> getBoundingBoxes() {
-    return new ResponseEntity<>(routingService.getBoundingBoxes(), HttpStatus.OK);
+  @GetMapping("/boxes")
+  public ResponseEntity<?> getBoundingBoxes(@RequestParam double minLon,
+      @RequestParam double maxLon,
+      @RequestParam double minLat, @RequestParam double maxLat) {
+    return new ResponseEntity<>(routingService.getBoundingBoxes(minLon, maxLon, minLat, maxLat),
+        HttpStatus.OK);
   }
 
 }
