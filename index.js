eebae2f9fe25f618ca00d799fc900e8c70f4e489
Han Nie <nie.han@northeastern.edu>
2024-10-29 23:16:13 -0700

    attempted to retrieve all points in a region from shadeMap API

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const puppeteer = require('puppeteer');
+import express from 'express';
+import puppeteer from 'puppeteer';
+import {data} from './data.js';
 
 const app = express();
 const port = 3000;
@@ -9,7 +10,11 @@ let browser;
 async function initializeBrowser() {
   if (!browser) {
     browser = await puppeteer.launch(
-        {headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        {
+          headless: false,
+          args: ['--no-sandbox', '--disable-setuid-sandbox'],
+          defaultViewport: null
+        });
   }
   return browser;
 }
@@ -18,7 +23,6 @@ app.use(express.json());
 
 app.post('/api/shade-profile', async (req, res) => {
   const {GeoJSON, dates} = req.body;
-
   try {
     const browser = await initializeBrowser();
     const page = await browser.newPage();
@@ -40,7 +44,6 @@ app.post('/api/shade-profile', async (req, res) => {
           margin: 0;
           padding: 0;
         }
-    
         html, body, #map {
           height: 100%;
         }
@@ -53,69 +56,50 @@ app.post('/api/shade-profile', async (req, res) => {
         const TILE_SIZE = 512;
         const BUILDING_ZOOM = 15;
     
-        const lng2pixel = (lng, zoom) => {
-          return ((lng + 180) / 360 * Math.pow(2, zoom)) * TILE_SIZE;
-        }
-    
         const geoJson = ${JSON.stringify(GeoJSON)}
-        console.log(geoJson)
-        const datess = ${JSON.stringify(dates)}.map(d => new Date(parseInt(d)));
-        console.log(datess)
+        const dateArr = ${JSON.stringify(dates)}.map(d => new Date(parseInt(d)));
     
-        const lat2pixel = (lat, zoom) => {
-          return ((1 - Math.log(Math.tan(lat * Math.PI / 180) + 1 / Math.cos(lat * Math.PI / 180))
-              / Math.PI) / 2 * Math.pow(2, zoom)) * TILE_SIZE
+        const numPoints = 10;
+        const boundingBoxes = ${JSON.stringify(data)}
+        
+        const groups = []
+        
+        for (let i = 0; i < 1; i++) {
+          let edges = boundingBoxes[i]
+          const group = []
+          edges.forEach(edge => {
+            const temp = []
+            let prev = edge.slice(0, 2)
+            let idx = 2
+            while (idx < edge.length - 1) {
+              let curr = edge.slice(idx, idx + 2);
+              let route = turf.lineString([prev, curr]);
+              let length = turf.length(route)
+              let step = length / numPoints;
+              let points = []
+              for (let j = 0; j < numPoints; j++) {
+                let point = turf.along(route, step * j)
+                points.push(point.geometry.coordinates)
+              }
+              temp.push(points)
+              prev = curr
+              idx += 2;
+            }
+            group.push(temp)
+          });
+          groups.push(group)
         }
-    
-        const unproject = (coords, zoom) => {
-          const [lng, lat] = coords;
-          return [lng2pixel(lng, zoom), lat2pixel(lat, zoom)];
-        }
-    
-        const numPoints = 500;
-        const busRoute = geoJson.geometry;
-        if (busRoute.type !== 'LineString') {
-          throw new Error('This example requires geometry of type LineString')
-        }
-        const length = turf.length(busRoute);
-    
-        const distanceBetweenPoints = length / numPoints;
-        const pointLocations = [];
-        for (let i = 0; i < numPoints; i++) {
-          const pointGeoJSON = turf.along(busRoute, distanceBetweenPoints * i);
-          pointLocations.push(pointGeoJSON.geometry.coordinates);
-        }
-        const viewportWidth = window.innerWidth * .8;
-        const viewportHeight = window.innerHeight * .8;
-    
-        const screenGroups = [];
-    
-        while (pointLocations.length > 0) {
-          const group = [pointLocations.shift()];
-          let groupPixelWidth = 0;
-          let groupPixelHeight = 0;
-          while (pointLocations.length > 0 && groupPixelWidth < viewportWidth && groupPixelHeight
-          < viewportHeight) {
-            group.push(pointLocations.shift());
-            const groupPoints = turf.lineString(group);
-            const [minLng, minLat, maxLng, maxLat] = turf.bbox(groupPoints);
-            const [minX, minY] = unproject([minLng, minLat], BUILDING_ZOOM);
-            const [maxX, maxY] = unproject([maxLng, maxLat], BUILDING_ZOOM);
-            groupPixelWidth = maxX - minX;
-            groupPixelHeight = minY - maxY;
-          }
-          screenGroups.push(group);
-        }
-    console.log(screenGroups)
+
     
         mapboxgl.accessToken = "pk.eyJ1IjoiaGFubm5pZTIiLCJhIjoiY20xeDZ1OTR0MHJqaDJxcTF0dWdsNjQxaCJ9.g_kbtpQtag20otKxXyyltg";
         const map = new mapboxgl.Map({
           container: 'map',
           zoom: 15,
-          center: screenGroups[0][0],
+          center: {lng: -122.980325, lat: 49.24353},
           style: 'mapbox://styles/mapbox/streets-v11',
           hash: true
         });
+        
         const mapLoaded = (map) => {
           return new Promise((res, rej) => {
             function cb() {
@@ -130,86 +114,71 @@ app.post('/api/shade-profile', async (req, res) => {
             cb();
           });
         };
-    
-    
-        map.on('load', async () => {
-          colors = ['red', 'green', 'blue'];
-          screenGroups.forEach((group, index) => {
-            const sourceId = \`source\${index}\`;
-            const layerId =  \`layer\${index}\`;
-            map.addSource(sourceId, {
-              type: 'geojson',
-              data: {
-                type: 'FeatureCollection',
-                features: [{
-                  type: 'Feature',
-                  geometry: {
-                    type: 'LineString',
-                    coordinates: group
-                  }
-                }]
-              }
-            })
-            map.addLayer({
-              id: layerId,
-              source: sourceId,
-              type: 'line',
-              'paint': {
-                'line-color': colors[index % 3],
-                'line-width': 3
-              }
-            })
-          })
-    
+       
+          map.on('load', async() => {
+          
+         
           const shadeMap = new ShadeMap({
             apiKey: "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Im5pZS5oYW5Abm9ydGhlYXN0ZXJuLmVkdSIsImNyZWF0ZWQiOjE3MjgyMDQ1MDQwNzYsImlhdCI6MTcyODIwNDUwNH0.Zh6h2jLM2oobag_KgItfsw7E3fHUExziwpbjA80mGbY",
             date: new Date(),
             terrainSource: {
-              maxZoom: 15,
-              tileSize: 256,
-              getSourceUrl: ({x, y, z}) => {
-                return \`https://s3.amazonaws.com/elevation-tiles-prod/terrarium/\${z}/\${x}/\${y}.png\`;
-      },
-          getElevation: ({r, g, b, a}) => {
-            return (r * 256 + g + b / 256) - 32768;
-          },
-              _overzoom: 18
-        },
-        getFeatures: async () => {
-      await mapLoaded(map);
-      return map.querySourceFeatures('composite', {sourceLayer: 'building'}).filter(
-          (feature) => {
-            return feature.properties && feature.properties.underground !== "true"
-                && (feature.properties.height || feature.properties.render_height)
-          });
-    },
-    }).addTo(map);
+                maxZoom: 15,
+                tileSize: 256,
+                getSourceUrl: ({x, y, z}) => {
+                  return \`https://s3.amazonaws.com/elevation-tiles-prod/terrarium/\${z}/\${x}/\${y}.png\`;
+                },
+                getElevation: ({r, g, b, a}) => {
+                  return (r * 256 + g + b / 256) - 32768;
+                },
+                _overzoom: 18,
+            },
+            getFeatures: async () => {
+                await mapLoaded(map)
+                const buildingData = map.querySourceFeatures('composite', { sourceLayer: 'building' }).filter((feature) => {
+                    return feature.properties && feature.properties.underground !== "true" && (feature.properties.height || feature.properties.render_height)
+                });
+                return buildingData;
+            },
+          }).addTo(map);
     
-    let accumulatedOutput = [];
-    for (let i = 0; i < screenGroups.length; i++) {
-      const group = screenGroups[i];
-      const groupCenter = turf.center(turf.points(group));
-    
-      const shadeMapLoaded = new Promise((res) => {
-        shadeMap.on('idle', res);
-      });
-    
-      map.setCenter(groupCenter.geometry.coordinates).setZoom(BUILDING_ZOOM);
-      const mapboxLoaded = mapLoaded(map);
-    
-      const locations = group.map(coord => ({lng: coord[0], lat: coord[1]}));
-    
-      await Promise.all([mapboxLoaded, shadeMapLoaded]);
-    
-      const output = shadeMap._generateShadeProfile({
-        locations,
-        dates:datess,
-        sunColor: [255, 255, 255, 255],
-        shadeColor: [0, 0, 0, 255]
-      });
-      accumulatedOutput.push(...Array.from(output));
-    }
-    window.shadeProfileOutput = accumulatedOutput;
+          let accumulatedOutput = [];
+          for (let i = 0; i < 1; i++) {
+            const group = groups[i];
+            const minLon = -122.98701;
+            const maxLon = -122.97363999999999;
+            const minLat = 49.24033;
+            const maxLat = 49.24673;
+            
+            const bbox = [minLon, minLat, maxLon, maxLat];
+            
+            const bboxPolygon = turf.bboxPolygon(bbox);
+            
+            const center = turf.center(bboxPolygon);
+          
+            const shadeMapLoaded = new Promise((res) => {
+              shadeMap.on('idle', res);
+            });
+          
+            map.setCenter(center.geometry.coordinates).setZoom(BUILDING_ZOOM);
+            console.log(center.geometry.coordinates)
+            const mapboxLoaded = mapLoaded(map);
+            console.log(group)
+          
+            const locations = group.flatMap(subEdge => 
+                subEdge.map(point => ({ lng: point[0], lat: point[1]}))
+              )
+            
+            await Promise.all([mapboxLoaded, shadeMapLoaded]);
+          
+            const output = shadeMap._generateShadeProfile({
+              locations,
+              dates:[new Date(1681455600000)],
+              sunColor: [255, 255, 255, 255],
+              shadeColor: [0, 0, 0, 255]
+            });
+            accumulatedOutput.push(...Array.from(output));
+          }
+          window.shadeProfileOutput = accumulatedOutput;
     });
     })
     </script>
@@ -230,7 +199,7 @@ app.post('/api/shade-profile', async (req, res) => {
       });
     });
 
-    await page.close();
+    // await page.close();
     res.json({shadeProfile});
   } catch (error) {
     console.error('Error generating shade profile:', error);
